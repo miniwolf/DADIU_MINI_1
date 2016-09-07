6186133f0f8fd72ca1e7f71cec62981ea1b54777
Laura <lauryxky@hotmail.com>
2016-09-07 16:32:03 +0200

    Initial commit to U.I. Timer and score scripts finished. CakesScript depending on Player.

---
+++ MiniGame1/Assets/scripts/HighscoreScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighscoreScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Text>().text = "Highscore: " + PlayerPrefs.GetInt("highscore");
+	}
+}
