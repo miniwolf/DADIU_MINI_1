5cfa33d5661f81d0e13b14e2ae3e5419162e0d74
Laura <lauryxky@hotmail.com>
2016-09-09 10:20:23 +0200

    Added player pick up for cakes and laundry. Changed Score and CakesText so they initialize the player prefs with value 0.

---
+++ MiniGame1/Assets/scripts/PlayerScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+// TODO implements interface
+public class PlayerScript : MonoBehaviour {
+	public int speed = 6;
+	public int angularSpeed = 120;
+	public Camera cam;
+
+	private NavMeshAgent agent;
+	private string cakeTag = "Cake";
+	private string laundryTag = "Laundry";
+	private Stopwatch timer;
+	public Score score;
+	public CakesText cakeText;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.speed = speed;
+		agent.angularSpeed = angularSpeed;
+	}
+
+	/// <summary>
+	/// Moves the player agent to a selected position
+	/// </summary>
+	/// <param name="pos">Position selected in the scene</param>
+	private void Move(Vector3 pos) {
+		RaycastHit hit;
+		if ( Physics.Raycast(cam.ScreenPointToRay(pos), out hit) ) {
+			agent.destination = hit.point;
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		foreach ( Touch touch in Input.touches ) {
+			Move(touch.position);
+		}
+
+		// TODO: currently only works on testing with right mouse button
+		// These lines are only for testing.
+		if ( Input.GetMouseButtonDown(1) ) {
+			Move(Input.mousePosition);
+		}
+	}
+
+	/// <summary>
+	/// Handles collisions with cake and laundry game objects
+	/// Cake: Adds the cake on top of the player and updates the score
+	/// Laundry: Removes the laundry and adds laundry score
+	/// </summary>
+	/// <param name="other">Other collider</param>
+	void OnCollisionEnter(Collision other) {
+		if ( other.gameObject.tag == cakeTag ) {
+			other.gameObject.SetActive(false); // TODO replace with Despawn method
+			cakeText.AddCake();
+		}
+
+		if ( other.gameObject.tag == laundryTag ) {
+			other.gameObject.SetActive(false); // TODO replace with Despawn method
+			score.AddLaundryScore();
+			// TODO speedup
+		}
+	}
+}
