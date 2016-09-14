d1f3511da7dea1011fc247b4a0437608d33f00db
blacou12 <blacou12@student.aau.dk>
2016-09-14 10:32:08 +0200

    UK and DK UI ready with changelanguage button

---
+++ FullGame/Assets/scripts/ChangeLanguage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeLanguage : MonoBehaviour {
+	public void ChangeLang(int currScene){
+		if (currScene == 1) {
+			//Change to English
+			SceneManager.LoadScene (0);
+		}else if(currScene == 0){
+			//Change to Danish
+			SceneManager.LoadScene (1);			
+		}
+
+
+
+	}
+}
