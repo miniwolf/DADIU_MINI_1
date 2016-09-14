63ec94ae0461461164a0472d914fec3a5e22f8fc
blacou12 <blacou12@student.aau.dk>
2016-09-14 10:53:08 +0200

    Fixed Multiple clicks on reload

--- FullGame/Assets/scripts/Reload.cs
+++ FullGame/Assets/scripts/Reload.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class Reload : MonoBehaviour {
+	bool hasBeenPressed;
+	void Start(){
+		hasBeenPressed = false;
+	}
 	public void ReloadLevel() {
-		PlayerPrefs.SetInt("shouldShowMenu", 0);
-		Application.LoadLevel(Application.loadedLevel);
+		if (!hasBeenPressed) {
+			PlayerPrefs.SetInt ("shouldShowMenu", 0);
+			Application.LoadLevel (Application.loadedLevel);
+		}
 	}
 }
